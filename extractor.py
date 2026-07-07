f52d4b5631f5ddb39223c01a828ed3ce4cc3ca24
Shahriyar Bhutto <88304298+ShahriyarBhutto@users.noreply.github.com>
2026-07-07 16:35:58 +0500

    App setup

---
+++ extractor.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import json, os
+import pdfplumber
+
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="",
+    api_key=os.environ[""]
+)
+
+def extract_text_from_pdf(file):
+    with pdfplumber.open(file) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+def get_structured_data(text):
+    prompt = f"{text}"
+
+    response = client.chat.completions.create(
+        model="",
+        messages=[{"role":"user","content":prompt}]
+    ) 
+    clean_text = response.choices[0].message.content.strip().replace("```json","").replace("```","")
+    return json.loads(clean_text)
